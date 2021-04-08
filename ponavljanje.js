556db6a663fc569ecf958c502b9c17fcb14417c4
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2021-04-08 18:49:04 +0200

    Add files via upload

--- ponavljanje.js
+++ ponavljanje.js
@@ -1,2 +1,25 @@
-console.log('radi');
+//console.log('radi');
 
+
+
+const hightMark = 1.88;
+const massMark = 95;
+
+const hightJohn = 1.88;
+const massJohn = 95;
+
+const markBMI = massMark / hightMark ** 2;
+const johnBMI = massJohn / hightJohn ** 2;
+console.log(markBMI, johnBMI);
+
+const higherBMI = markBMI > johnBMI;
+
+console.log(higherBMI);
+
+if (markBMI > johnBMI) {
+    console.log('Mark has higher BMI');
+} else if (markBMI === johnBMI) {
+    console.log('Theirs BMI is equal');
+} else {
+    console.log('john has higher BMI');
+}
