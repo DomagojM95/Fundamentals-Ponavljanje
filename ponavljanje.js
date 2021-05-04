be58fb084780c60d98a38e922ecc1a7e82297f75
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2021-05-04 22:20:15 +0200

    Add files via upload

--- ponavljanje.js
+++ ponavljanje.js
@@ -419,7 +419,7 @@ for (let i = 0; i < domagoj.length; i++) {
 
 }
 
-*/
+
 
 const domagoj = [
     'Domagoj',
@@ -442,7 +442,7 @@ for (let execersise = 1; execersise < 4; execersise++) {
 }
 
 
-/*
+
 for (let rep = 1; rep <= 10; rep++) {
     //console.log(`lifting weights ${rep}`);
 };
@@ -463,3 +463,30 @@ while (dice !== 6) {
     };
 };
 */
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+
+
